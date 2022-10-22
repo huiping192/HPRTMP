50ca3e1bbe2a7091d02ddfb9161d2389ebf4189d
huiping_guo <huiping.guo@dena.jp>
2022-10-22 14:03:37 +0900

    add exsample

--- Package.swift
+++ Package.swift
@@ -5,6 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "HPRTMP",
+    platforms: [.iOS(.v13),.macOS(.v10_10)],
+    products: [
+        .library(
+            name: "HPRTMP",
+            targets: ["HPRTMP"]
+        ),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -12,7 +19,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
+        .target(
             name: "HPRTMP",
             dependencies: []),
         .testTarget(
--- Sources/HPRTMP/main.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-let url = URL(string: "rtmp://10.123.2.141:1935/stream")!
-let streamKey = "hello"
-let port = 1935
-let socket = RTMPSocket(streamURL: url, streamKey: streamKey, port: port)
-socket?.resume()
