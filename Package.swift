d448bbcc77d12fef53d3521cdcead02b0b9f3d10
Huiping Guo <huiping192@gmail.com>
2023-03-20 15:28:46 +0900

    fix swift build error

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "HPRTMP",
-    platforms: [.iOS(.v13),.macOS(.v10_10)],
+    platforms: [.iOS(.v13),.macOS(.v10_15)],
     products: [
         .library(
             name: "HPRTMP",
