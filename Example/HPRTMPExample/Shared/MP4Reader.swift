8779abe8b40ca2bcda852ebb757be691164ba3c9
Huiping Guo <huiping192@gmail.com>
2023-06-24 15:09:41 +0900

    sample update

--- Example/HPRTMPExample/Shared/MP4Reader.swift
+++ Example/HPRTMPExample/Shared/MP4Reader.swift
@@ -7,19 +7,50 @@
 
 import Foundation
 import AVFoundation
+import HPRTMP
 
 struct MP4Reader {
   
   private let url: URL
   
   private var assetReader: AVAssetReader?
+  private var audioReaderOutput: AVAssetReaderTrackOutput?
+  private var videoReaderOutput: AVAssetReaderTrackOutput?
   
+  var sendVideoBuffer: ((Data,Bool,UInt64,Int32) -> Void)?
+  var sendAudioBuffer: ((Data,Data,UInt64) -> Void)?
+  
+  var sendVideoHeader: ((Data) -> Void)?
+  var sendAudioHeader: ((Data) -> Void)?
+  
+  var isVideoHeaderSended = false
+  var isAudioHeaderSended = false
+
   init(url: URL) {
     self.url = url
+    
+    processMP4()
   }
   
-  func start() {
+  mutating func start() {
     assetReader?.startReading()
+    
+    while let videoSampleBuffer = videoReaderOutput?.copyNextSampleBuffer() {
+      if !isVideoHeaderSended {
+        isVideoHeaderSended = true
+        getVideoHeader(buffer: videoSampleBuffer)
+      }
+      handleVideoBuffer(buffer: videoSampleBuffer)
+    }
+    
+    while let audioSampleBuffer = audioReaderOutput?.copyNextSampleBuffer() {
+      if !isAudioHeaderSended {
+        isAudioHeaderSended = true
+        getAudioHeader(buffer: audioSampleBuffer)
+      }
+      handleAudioBuffer(buffer: audioSampleBuffer)
+    }
+    
   }
   
   mutating func processMP4() {
@@ -35,49 +66,240 @@ struct MP4Reader {
     assetReader.add(videoReaderOutput)
     
     let audioTrack = asset.tracks(withMediaType: .audio).first
-        let audioReaderOutput = AVAssetReaderTrackOutput(track: audioTrack!, outputSettings: nil)
-        assetReader.add(audioReaderOutput)
-    
-    
-    while let videoSampleBuffer = videoReaderOutput.copyNextSampleBuffer() {
-      handleVideoBuffer(buffer: videoSampleBuffer)
-    }
-    
-    while let audioSampleBuffer = audioReaderOutput.copyNextSampleBuffer() {
-      handleAudioBuffer(buffer: audioSampleBuffer)
-    }
+    let audioReaderOutput = AVAssetReaderTrackOutput(track: audioTrack!, outputSettings: nil)
+    assetReader.add(audioReaderOutput)
     
     self.assetReader = assetReader
+    self.audioReaderOutput = audioReaderOutput
+    self.videoReaderOutput = videoReaderOutput
   }
   
   private func handleVideoBuffer(buffer: CMSampleBuffer ) {
+    guard let bufferData = CMSampleBufferGetDataBuffer(buffer)?.data else {
+      return
+    }
     
+    guard let attachments = CMSampleBufferGetSampleAttachmentsArray(buffer, createIfNecessary: true) as? NSArray else { return }
+    
+    guard let attachment = attachments[0] as? NSDictionary else {
+      return
+    }
+    
+    let isKeyframe = !(attachment[kCMSampleAttachmentKey_DependsOnOthers] as? Bool ?? true)
+    
+    let presentationTimeStamp = buffer.presentationTimeStamp
+    var decodeTimeStamp = buffer.decodeTimeStamp
+    if decodeTimeStamp == .invalid {
+      decodeTimeStamp = presentationTimeStamp
+    }
+    let timestamp = UInt64(presentationTimeStamp.seconds * 1000)
+    let compositionTime = Int32((presentationTimeStamp.seconds - decodeTimeStamp.seconds) * 1000)
+    
+    self.sendVideoBuffer?(bufferData,isKeyframe,timestamp,compositionTime)
   }
   
   private func handleAudioBuffer(buffer: CMSampleBuffer) {
+    guard let aacHeader = aacHeader else { return }
+    guard let data = getAACData(from: buffer) else { return }
+        
+    let timestamp = buffer.presentationTimeStamp.seconds.isFinite ? UInt64(buffer.presentationTimeStamp.seconds * 1000) : 0
+    sendAudioBuffer?(data, aacHeader, timestamp)
+  }
+  
+  func getAACData(from sampleBuffer: CMSampleBuffer) -> Data? {
+    guard let blockBuffer = CMSampleBufferGetDataBuffer(sampleBuffer) else {
+      print("Could not get block buffer from sample buffer")
+      return nil
+    }
     
+    let length = CMBlockBufferGetDataLength(blockBuffer)
+    var dataPointer: UnsafeMutablePointer<Int8>? = nil
+    
+    CMBlockBufferGetDataPointer(blockBuffer, atOffset: 0, lengthAtOffsetOut: nil, totalLengthOut: nil, dataPointerOut: &dataPointer)
+    
+    if let dataPointer = dataPointer {
+      return Data(bytes: dataPointer, count: length)
+    }
+    
+    return nil
+  }
+  
+  
+  private mutating func getAudioHeader(buffer: CMSampleBuffer) {
+    guard let formatDescription = CMSampleBufferGetFormatDescription(buffer) else {
+        print("Could not get format description")
+        return
+    }
+
+    // Get the ASBD from the format description
+    guard var asbd = CMAudioFormatDescriptionGetStreamBasicDescription(formatDescription)?.pointee else { return }
+    var outFormatDescription: CMFormatDescription?
+    CMAudioFormatDescriptionCreate(allocator: kCFAllocatorDefault, asbd: &asbd, layoutSize: 0, layout: nil, magicCookieSize: 0, magicCookie: nil, extensions: nil, formatDescriptionOut: &outFormatDescription)
+
+
+    guard let streamBasicDesc = outFormatDescription?.streamBasicDesc,
+          let mp4Id = MPEG4ObjectID(rawValue: Int(streamBasicDesc.mFormatFlags)) else {
+      return
+    }
+    var descData = Data()
+    let config = AudioSpecificConfig(objectType: mp4Id,
+                                     channelConfig: ChannelConfigType(rawValue: UInt8(streamBasicDesc.mChannelsPerFrame)),
+                                     frequencyType: SampleFrequencyType(value: streamBasicDesc.mSampleRate))
+    
+    let aacHeader = aacHeader(outFormatDescription: outFormatDescription)
+    self.aacHeader = aacHeader
+    descData.append(aacHeader)
+    descData.write(AudioData.AACPacketType.header.rawValue)
+    descData.append(config.encodeData)
+    self.sendAudioHeader?(descData)
+  }
+  
+  private var aacHeader: Data?
+  
+  /*
+   Sound format: a 4-bit field that indicates the audio format, such as AAC or MP3.
+   Sound rate: a 2-bit field that indicates the audio sample rate, such as 44.1 kHz or 48 kHz.
+   Sound size: a 1-bit field that indicates the audio sample size, such as 16-bit or 8-bit.
+   Sound type: a 1-bit field that indicates the audio channel configuration, such as stereo or mono.
+   */
+  func aacHeader(outFormatDescription: CMFormatDescription?) -> Data {
+    guard let desc = outFormatDescription,
+          let streamBasicDesc = desc.streamBasicDesc else {
+      return Data()
+    }
+    let value = (AudioData.SoundFormat.aac.rawValue << 4 |
+                 AudioData.SoundRate(value: streamBasicDesc.mSampleRate).rawValue << 2 |
+                 AudioData.SoundSize.snd16Bit.rawValue << 1 |
+                 AudioData.SoundType.sndStereo.rawValue)
+    return Data([UInt8(value)])
   }
   
   private func getVideoHeader(buffer: CMSampleBuffer) {
     // SPS & PPS data
     guard let formatDescription = CMSampleBufferGetFormatDescription(buffer) else { return }
-    let parameterSetCount = CMVideoFormatDescriptionGetH264ParameterSetAtIndex(formatDescription, parameterSetIndex: 0, parameterSetPointerOut: nil, parameterSetSizeOut: nil, parameterSetCountOut: nil, nalUnitHeaderLengthOut: nil)
     
-    if parameterSetCount > 2 {
-      var pointerSPS: UnsafePointer<UInt8>?
-      var pointerPPS: UnsafePointer<UInt8>?
-      var sizeSPS: size_t = 0
-      var sizePPS: size_t = 0
-      var lengthSPS: Int32 = 0
-      var lengthPPS: Int32 = 0
-      
-      CMVideoFormatDescriptionGetH264ParameterSetAtIndex(formatDescription, parameterSetIndex: 0, parameterSetPointerOut: &pointerSPS, parameterSetSizeOut: &sizeSPS, parameterSetCountOut: nil, nalUnitHeaderLengthOut: &lengthSPS)
-      
-      CMVideoFormatDescriptionGetH264ParameterSetAtIndex(formatDescription, parameterSetIndex: 1, parameterSetPointerOut: &pointerPPS, parameterSetSizeOut: &sizePPS, parameterSetCountOut: nil, nalUnitHeaderLengthOut: &lengthPPS)
-      
-      // Now you have your SPS & PPS data:
-      let spsData = Data(bytes: pointerSPS!, count: sizeSPS)
-      let ppsData = Data(bytes: pointerPPS!, count: sizePPS)
+    
+    var pointerSPS: UnsafePointer<UInt8>?
+    var pointerPPS: UnsafePointer<UInt8>?
+    var sizeSPS: size_t = 0
+    var sizePPS: size_t = 0
+    var lengthSPS: Int32 = 0
+    var lengthPPS: Int32 = 0
+    
+    CMVideoFormatDescriptionGetH264ParameterSetAtIndex(formatDescription, parameterSetIndex: 0, parameterSetPointerOut: &pointerSPS, parameterSetSizeOut: &sizeSPS, parameterSetCountOut: nil, nalUnitHeaderLengthOut: &lengthSPS)
+    
+    CMVideoFormatDescriptionGetH264ParameterSetAtIndex(formatDescription, parameterSetIndex: 1, parameterSetPointerOut: &pointerPPS, parameterSetSizeOut: &sizePPS, parameterSetCountOut: nil, nalUnitHeaderLengthOut: &lengthPPS)
+    
+    // Now you have your SPS & PPS data:
+    let sps = Data(bytes: pointerSPS!, count: sizeSPS)
+    let pps = Data(bytes: pointerPPS!, count: sizePPS)
+
+    var body = Data()
+    body.append(Data([0x17]))
+    body.append(Data([0x00]))
+    
+    body.append(Data([0x00, 0x00, 0x00]))
+    
+    body.append(Data([0x01]))
+    
+    let spsSize = sps.count
+    
+    body.append(Data([sps[1], sps[2], sps[3], 0xff]))
+    
+    /*sps*/
+    body.append(Data([0xe1]))
+    body.append(Data([(UInt8(spsSize) >> 8) & 0xff, UInt8(spsSize) & 0xff]))
+    body.append(Data(sps))
+    
+    let ppsSize = pps.count
+    
+    /*pps*/
+    body.append(Data([0x01]))
+    body.append(Data([(UInt8(ppsSize) >> 8) & 0xff, UInt8(ppsSize) & 0xff]))
+    body.append(Data(pps))
+    
+    sendVideoHeader?(body)
+  }
+  
+}
+
+
+
+extension CMBlockBuffer {
+  var data: Data? {
+    var length: Int = 0
+    var pointer: UnsafeMutablePointer<Int8>?
+    guard CMBlockBufferGetDataPointer(self, atOffset: 0, lengthAtOffsetOut: nil, totalLengthOut: &length, dataPointerOut: &pointer) == noErr,
+          let p = pointer else {
+      return nil
+    }
+    return Data(bytes: p, count: length)
+  }
+  
+  var length: Int {
+    return CMBlockBufferGetDataLength(self)
+  }
+  
+}
+
+extension CMAudioFormatDescription {
+  
+  var streamBasicDesc: AudioStreamBasicDescription? {
+    get {
+      return CMAudioFormatDescriptionGetStreamBasicDescription(self)?.pointee
+    }
+  }
+}
+
+
+/*
+ AudioSpecificConfig = 2 bytes,
+ number = bits
+ ------------------------------
+ | audioObjectType (5)        |
+ | sampleingFrequencyIndex (4)|
+ | channelConfigration (4)    |
+ | frameLengthFlag (1)        |
+ | dependsOnCoreCoder (1)     |
+ | extensionFlag (1)          |
+ ------------------------------
+ */
+struct AudioSpecificConfig {
+  let objectType: MPEG4ObjectID
+  var channelConfig: ChannelConfigType = .unknown
+  var frequencyType: SampleFrequencyType = .unknown
+  let frameLengthFlag: Bool
+  let dependsOnCoreCoder: UInt8
+  let extensionFlag: UInt8
+  init (data: Data) {
+    self.objectType = MPEG4ObjectID(rawValue: Int((0b11111000 & data[0]) >> 3)) ?? .aac_Main
+    self.frequencyType = SampleFrequencyType(rawValue: (0b00000111 & data[0]) << 1 | (0b10000000 & data[1]) >> 7)
+    self.channelConfig = ChannelConfigType(rawValue: (0b01111000 & data[1]) >> 3)
+    let value = UInt8(data[1] & 0b00100000) == 1
+    self.frameLengthFlag = value
+    self.dependsOnCoreCoder = data[1] & 0b000000010
+    self.extensionFlag = data[1] & 0b000000001
+  }
+  
+  init(objectType: MPEG4ObjectID, channelConfig: ChannelConfigType, frequencyType: SampleFrequencyType, frameLengthFlag: Bool = false, dependsOnCoreCoder: UInt8 = 0, extensionFlag: UInt8 = 0) {
+    self.objectType = objectType
+    self.channelConfig = channelConfig
+    self.frequencyType = frequencyType
+    self.frameLengthFlag = frameLengthFlag
+    self.dependsOnCoreCoder = dependsOnCoreCoder
+    self.extensionFlag = extensionFlag
+  }
+  
+  var encodeData: Data {
+    get {
+      let flag = self.frameLengthFlag ? 1 : 0
+      let first = UInt8(self.objectType.rawValue) << 3 | UInt8(self.frequencyType.rawValue >> 1 & 0b00000111)
+      let second = (0b10000000 & self.frequencyType.rawValue << 7) |
+      (0b01111000 & self.channelConfig.rawValue << 3) |
+      (UInt8(flag) << 2) |
+      (self.dependsOnCoreCoder << 1) |
+      self.extensionFlag
+      return Data([first, second])
     }
   }
 }
--- Example/HPRTMPExample/Shared/RTMPService.swift
+++ Example/HPRTMPExample/Shared/RTMPService.swift
@@ -8,19 +8,82 @@
 import Foundation
 import HPRTMP
 
-class RTMPService {
+actor RTMPService: RTMPPublishSessionDelegate {
+  func sessionStatusChange(_ session: HPRTMP.RTMPPublishSession, status: HPRTMP.RTMPPublishSession.Status) {
+    if status == .publishStart {
+      reader.start()
+    }
+  }
+  
+  func sessionError(_ session: HPRTMP.RTMPPublishSession, error: HPRTMP.RTMPError) {
+    
+  }
+  
   
   private var session = RTMPPublishSession()
+  
+  var reader: MP4Reader
+  
+  private var lastVideoTimestamp: UInt64 = 0
+  private var lastAudioTimestamp: UInt64 = 0
   
   init() {
 //    let url = URL(string: "rtmp://192.168.11.23/live")!
 //    let streamKey = "hello"
 //    let port = 1935
 //    socket.connect(streamURL: url, streamKey: streamKey, port: port)
+    let url = Bundle.main.url(forResource: "720p", withExtension: "mp4")!
+    reader = MP4Reader(url: url)
   }
   
   func run() async {
+    
+    await session.setDelegate(self)
     let publishConfig = PublishConfigure(width: 640, height: 480, videocodecid: VideoData.CodecId.avc.rawValue, audiocodecid: AudioData.SoundFormat.aac.rawValue, framerate: 30, videoDatarate: 30, audioDatarate: nil, audioSamplerate: nil)
     await session.publish(url: "rtmp://192.168.11.23/live/haha", configure: publishConfig)
+    
+    
+    reader.sendAudioHeader = { data in
+      Task {
+        await self.session.publishAudioHeader(data: data)
+      }
+    }
+    
+    reader.sendVideoHeader = { data in
+      Task {
+        await self.session.publishVideoHeader(data: data, time: 0)
+      }
+    }
+    
+    reader.sendAudioBuffer = { data,aacHeader,timestamp in
+      Task {
+        var audioPacketData = Data()
+        audioPacketData.append(aacHeader)
+        audioPacketData.write(AudioData.AACPacketType.raw.rawValue)
+        audioPacketData.append(data)
+        let delta = UInt32(timestamp - self.lastAudioTimestamp)
+        await self.session.publishAudio(data: audioPacketData, delta: delta)
+      }
+    }
+    
+    reader.sendVideoBuffer = { data,isKeyFrame,timestamp,compositionTime in
+      Task {
+        var descData = Data()
+        let frameType = isKeyFrame ? VideoData.FrameType.keyframe : VideoData.FrameType.inter
+        let frameAndCode:UInt8 = UInt8(frameType.rawValue << 4 | VideoData.CodecId.avc.rawValue)
+        descData.append(Data([frameAndCode]))
+        descData.append(Data([VideoData.AVCPacketType.nalu.rawValue]))
+        
+        let delta = timestamp - self.lastVideoTimestamp
+        // 24bit
+        descData.write24(compositionTime, bigEndian: true)
+        descData.append(data)
+        await self.session.publishVideo(data: descData, delta: UInt32(delta))
+        self.lastVideoTimestamp = timestamp
+      }
+    }
+    
+    
+    
   }
 }
