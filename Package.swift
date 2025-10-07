d79d7021c7a6efbe4652a655c7565c6b927d3331
Huiping Guo <huiping192@gmail.com>
2025-10-07 19:33:54 +0900

    Add NIOSSL dependency for RTMPS support

--- Package.swift
+++ Package.swift
@@ -14,12 +14,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl", from: "2.0.0"),
     ],
     targets: [
         .target(
             name: "HPRTMP",
             dependencies: [
-                .product(name: "NIO", package: "swift-nio")
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "NIOSSL", package: "swift-nio-ssl")
             ],
             swiftSettings: [
                 .enableUpcomingFeature("StrictConcurrency")
