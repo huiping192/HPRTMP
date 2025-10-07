e8213890c74eead19b89486e03107e20731afbe5
Huiping Guo <huiping192@gmail.com>
2025-10-07 19:34:09 +0900

    Add TLS support to network client with SSL handler

--- Sources/HPRTMP/Network/NetworkConnectable.swift
+++ Sources/HPRTMP/Network/NetworkConnectable.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 protocol NetworkConnectable: Sendable {
-  func connect(host: String, port: Int) async throws
+  func connect(host: String, port: Int, enableTLS: Bool) async throws
   func sendData(_ data: Data) async throws
   func receiveData() async throws -> Data
   func close() async throws
